e6dd882abc2edc38296b4a6e80777fdcdbc0b641
Vadim Geroim <vgeroim.qa@gmail.com>
2024-09-14 17:16:11 -0400

    Define the Patient struct

--- main.go
+++ main.go
@@ -6,6 +6,13 @@ import (
 	"net/http"
 )
 
+// Patient data
+type Patient struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
 func main() {
 	http.HandleFunc("/", homePage)
 	fmt.Println("Server is running on port 8080...")
